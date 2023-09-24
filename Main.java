6f32fb6972527c92dc9c35d70fe36b548f015620
Crl-Vlz <crlvlz0215@gmail.com>
2023-09-24 13:53:17 -0600

    Fices running to waiting transition

--- Main.java
+++ Main.java
@@ -3,19 +3,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.SwingUtilities;
 
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main { //mike_features
+public class Main { // mike_features
 
     private JFrame window;
     private Thread ths[];
     private static int nThreads = 0;
 
-    private int sizeX = 800;    
+    private int sizeX = 800;
     private int sizeY = 400;
 
     public Main() {
--- StateA.java
+++ StateA.java
@@ -27,8 +27,8 @@ public class StateA extends Thread {
 
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        imgLabel= new JLabel();
-        
+        imgLabel = new JLabel();
+
         JButton btnStart = new JButton("Start");
         JButton btnWait = new JButton("Wait");
         JButton btnStop = new JButton("Stop");
@@ -78,24 +78,30 @@ public class StateA extends Thread {
         btnWait.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                imgLabel.setIcon(imgSleeping);
                 int nTime = Integer.parseInt(text.getText());
                 SwingUtilities.invokeLater(() -> {
+                    imgLabel.setIcon(imgSleeping);
                     btnStop.setEnabled(false);
                     btnWait.setEnabled(false);
                     sp.revalidate();
                 });
-                try {
-                    Thread.sleep(nTime);
-                } catch (InterruptedException err) {
-                    System.err.println(err.getMessage());
-                }
-                //imgLabel.setIcon(imgRunning);
-                SwingUtilities.invokeLater(() -> {
-                    btnStop.setEnabled(true);
-                    btnWait.setEnabled(true);
-                    sp.revalidate();
-                });
+
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(nTime);
+                    } catch (InterruptedException err) {
+                        System.err.println(err.getMessage());
+                    }
+
+                    SwingUtilities.invokeLater(() -> {
+                        imgLabel.setIcon(imgRunning);
+                        btnStop.setEnabled(true);
+                        btnWait.setEnabled(true);
+                        sp.revalidate();
+                    });
+
+                }).start();
+
             }
 
         });
